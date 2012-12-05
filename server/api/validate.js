639238e660e73c5432bce245cd41b2cb5dc4dddc
jkenoyer <kenoyer130@gmail.com>
2012-12-04 22:54:52 -0500

    db stab

--- server/api/validate.js
+++ server/api/validate.js
@@ -1,12 +1,23 @@
 var Cookies = require("Cookies");
+var MongoClient = require('mongodb').MongoClient;
 
-function execute(args, callback, request,response) {
+function execute(args, callback, request, response) {
 
-    if(args.login=="kenoyer130" && args.password=="fragile"){
-        var cookies = new Cookies(request, response);
-        cookies.set("site","site",{ httpOnly: false });
-        callback();
-    }
+    MongoClient.connect("mongodb://localhost:27017/test", function (err, db) {
+        if (err) {
+            return console.dir(err);
+        }
+
+        var accounts = db.collection('account');
+
+        var count = accounts.find({ "login":args.login, "password":args.password}).toArray(function (items) {
+            if (items != null) {
+                var cookies = new Cookies(request, response);
+                cookies.set("site", "site", { httpOnly:false });
+                callback();
+            }
+        });
+    });
 
     callback("Invalid login");
 }
