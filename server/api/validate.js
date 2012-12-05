8902b818fda2fdf612d6ed8262d1196f61577f3c
kenoyer130 <kenoyer130@gmail.com>
2012-12-04 22:24:21 -0500

    Update server/api/validate.js

--- server/api/validate.js
+++ server/api/validate.js
@@ -2,7 +2,7 @@ var Cookies = require("Cookies");
 
 function execute(args, callback, request,response) {
 
-    if(args.login=="kenoyer130" && args.password=="fragile"){
+    if(args.login=="kenoyer130" && args.password=="!!"){
         var cookies = new Cookies(request, response);
         cookies.set("site","site",{ httpOnly: false });
         callback();
